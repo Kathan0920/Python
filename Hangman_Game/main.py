99e256cac8b540b0b8f5aa8ebe48f1926d94013e
Kathan <shahkathan50@gmail.com>
2023-04-09 15:24:02 +0530

    This Game is currently in developing state

---
+++ Hangman_Game/main.py
@@ -0,0 +1,31 @@
+import random
+
+word_list = ["aardvark", "baboon", "camel"]
+chosen_word = random.choice(word_list)
+
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+#Create blanks
+display = []
+for i in range(len(chosen_word)):
+    display += '_'
+
+
+print(''.join(display))
+print("\n")
+
+Blank_remaining = display.count('_')
+
+while Blank_remaining > 0:
+    user_guess = input("Guess a letter: ").lower()
+    
+    for i in range(len(chosen_word)):
+        if user_guess == chosen_word[i]:
+            display[i] = chosen_word[i] 
+            
+    Blank_remaining = display.count('_')
+
+
+print(''.join(display))
+print("You won")
