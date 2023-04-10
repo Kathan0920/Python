1560624773aa38a3da34fa426f44e32cb005467b
Kathan <shahkathan50@gmail.com>
2023-04-10 23:16:37 +0530

    commenting another approach

--- Hangman_Game/main.py
+++ Hangman_Game/main.py
@@ -29,3 +29,18 @@ while Blank_remaining > 0:
 
 print(''.join(display))
 print("You won")
+
+# You can also do like:
+# end_of_game = False
+# while not end_of_game:
+#     user_guess = input("Guess a letter: ").lower()
+#    
+#     for i in range(len(chosen_word)):
+#         if user_guess == chosen_word[i]:
+#             display[i] = chosen_word[i]
+#     
+#     print(''.join(display)) 
+#     if "_" not in display:
+#         end_of_game = True
+#          print("You won")
+
