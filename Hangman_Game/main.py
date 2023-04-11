9d65ded5249da0aa5a76d5d0162cacda3a3582a1
Kathan <shahkathan50@gmail.com>
2023-04-11 23:26:12 +0530

    Completed the last 2nd step of this game.

--- Hangman_Game/main.py
+++ Hangman_Game/main.py
@@ -3,44 +3,39 @@ import random
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 
-#Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
-#Create blanks
 display = []
 for i in range(len(chosen_word)):
     display += '_'
 
 
-print(''.join(display))
-print("\n")
-
+lives = 6
+correct_guess = False
 Blank_remaining = display.count('_')
 
+
 while Blank_remaining > 0:
-    user_guess = input("Guess a letter: ").lower()
-    
+    user_guess = input("\nGuess a letter: ").lower()
+
+    correct_guess = False
     for i in range(len(chosen_word)):
         if user_guess == chosen_word[i]:
-            display[i] = chosen_word[i] 
+            display[i] = chosen_word[i]
+            correct_guess = True
             
+    
+    if not correct_guess:
+        lives -= 1
     Blank_remaining = display.count('_')
+    
+    print(f"{' '.join(display)}")
 
 
-print(''.join(display))
-print("You won")
+    if "_" not in display:
+        print("\nYou win!")
 
-# You can also do like:
-# end_of_game = False
-# while not end_of_game:
-#     user_guess = input("Guess a letter: ").lower()
-#    
-#     for i in range(len(chosen_word)):
-#         if user_guess == chosen_word[i]:
-#             display[i] = chosen_word[i]
-#     
-#     print(''.join(display)) 
-#     if "_" not in display:
-#         end_of_game = True
-#          print("You won")
-
+    if lives == 0:
+        print("\nYou lose!")
+        break
+    
