8c32b405e84525d975cccd638c308f7f27d68a3c
Kathan <shahkathan50@gmail.com>
2023-04-17 20:20:01 +0530

    Combined the  encryption and decryption function into one.

--- Caesar_Cipher/main.py
+++ Caesar_Cipher/main.py
@@ -1,28 +1,20 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
+alphabet = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+]
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-
-def encrypts(original_text,shift_no):
-    cipher_text = ""
-    for i in range(0,len(original_text)):
-        position = alphabet.index(original_text[i])
+def caesar(entered_text, shift_no, user_choice):
+    converted_text = ""
+    if user_choice == "decode":
+        shift_no = -shift_no
+    for i in range(0, len(entered_text)):
+        position = alphabet.index(entered_text[i])
         new_position = (position + shift_no) % 26
-        cipher_text += alphabet[new_position]
-    print(f"The encoded message is {cipher_text}")
-    
-def decrypts(cipher_text, shift_no):
-    original_msg = ""
-    for i in range(0,len(cipher_text)):
-        position = alphabet.index(cipher_text[i])
-        new_position = (position - shift_no) % 26
-        original_msg += alphabet[new_position]
-    print(f"The decoded message is {original_msg}")
-    
-if direction == "encode":
-    encrypts(original_text = text, shift_no = shift)
-else:
-    decrypts(cipher_text = text, shift_no = shift)
+        converted_text += alphabet[new_position]
+    print(f"The {user_choice}d message is {converted_text}")
+
+caesar(entered_text=text, shift_no=shift, user_choice=direction)
